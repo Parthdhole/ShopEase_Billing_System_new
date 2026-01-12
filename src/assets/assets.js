4195bf50b73fa652bae791b28de24f5aab457c50
Parthdhole <parthdhole405@gmail.com>
2026-01-12 23:03:21 +0530

    Update assets and remove unused category service

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,11 @@
  import logo from './logo.png';
 import upload from './upload.png';
-import login from './login-bg.png';
+import login from './login-bg.png'
+import Profile from './Profile.jpg';
 export const assets ={
    logo,
    upload,
-   login
+   login,
+   Profile
 
 }
